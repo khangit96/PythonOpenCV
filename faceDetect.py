22eb5057c09613c31b41ddda11e0cb866ab2603d
khangit96 <khangit69@gmail.com>
2018-01-15 09:31:52 +0700

    The second commit

--- faceDetect.py
+++ faceDetect.py
@@ -4,7 +4,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-img = cv2.imread('/home/khang/Downloads/linh14.jpg')
+img = cv2.imread('/home/khang/Downloads/truonggiang.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
@@ -18,6 +18,8 @@ for (x,y,w,h) in faces:
         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
 print('Face {0}'.format(len(faces)))
+cv2.namedWindow('img',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('img', 600,600)
 cv2.imshow('img',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
