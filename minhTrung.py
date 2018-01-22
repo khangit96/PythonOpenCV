9a4001107d94d818a27145037aed5349fcded3b1
khangit96 <khangit69@gmail.com>
2018-01-22 21:40:09 +0700

    flflf

--- minhTrung.py
+++ minhTrung.py
@@ -1,5 +1,5 @@
 import pyrebase
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 import threading
 
@@ -15,84 +15,64 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
 #init GPIO
-GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
 
-GPIO.setwarnings(False)
+# GPIO.setwarnings(False)
 
-GPIO.setup(13,GPIO.OUT)
-GPIO.setup(6,GPIO.OUT)
-GPIO.setup(5,GPIO.OUT)
-GPIO.setup(11,GPIO.OUT)
-GPIO.setup(9,GPIO.OUT)
-GPIO.setup(10,GPIO.OUT) #relay3
-GPIO.setup(22,GPIO.OUT) #relay2
-GPIO.setup(27,GPIO.OUT) #relay1
+# GPIO.setup(13,GPIO.OUT)
+# GPIO.setup(6,GPIO.OUT)
+# GPIO.setup(5,GPIO.OUT)
+# GPIO.setup(11,GPIO.OUT)
 
-GPIO.output(13,GPIO.LOW)
-GPIO.output(6,GPIO.LOW)
-GPIO.output(5,GPIO.LOW)
-GPIO.output(11,GPIO.LOW)
-GPIO.output(9,GPIO.LOW)
-GPIO.output(10,GPIO.LOW)
-GPIO.output(22,GPIO.LOW)
-GPIO.output(27,GPIO.LOW)
+# GPIO.setup(9,GPIO.OUT)
+# GPIO.setup(10,GPIO.OUT) #relay3
+# GPIO.setup(22,GPIO.OUT) #relay2
+# GPIO.setup(27,GPIO.OUT) #relay1
+
+# GPIO.output(13,GPIO.LOW)
+# GPIO.output(6,GPIO.LOW)
+# GPIO.output(5,GPIO.LOW)
+# GPIO.output(11,GPIO.LOW)
+# GPIO.output(9,GPIO.LOW)
+# GPIO.output(10,GPIO.LOW)
+# GPIO.output(22,GPIO.LOW)
+# GPIO.output(27,GPIO.LOW)
 
 #Thread
-count1=0
-count2=0
+checkRelay1=True
+checkRelay2=True
 
-class Relay1Thread (threading.Thread):
-     def __init__(self,threadID, name):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-
-     def run(self):
-        blinkRelay1(self.name)
-
-class Relay2Thread (threading.Thread):
-     def __init__(self,threadID, name):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-
-     def run(self):
-        blinkRelay2(self.name)
-
-def blinkRelay1(threadName):
-   global count1
-   while count1<=8:
-      print('relay 1')
-      count1+=1
-      time.sleep(1)
-
-def blinkRelay2(threadName):
-       global count2
-       while count2<=8:
-        print('relay 2')
-        count2+=1
+def worker1():
+    global checkRelay1
+    while checkRelay1==True:
+        print('worker1: '+str(checkRelay1))
         time.sleep(1)
 
-# Create new threads
-
+def worker2():
+    global checkRelay2
+    while checkRelay2==True:
+        print('worker2: '+str(checkRelay2))
+        time.sleep(1)
 #Relay1
 def stream_relay1(message):
       if message['path']== '/TurnOn':
           if message['data']==True:
-              GPIO.output(27,GPIO.HIGH)
+              #GPIO.output(27,GPIO.HIGH)
               print('Relay1-TurnOn-true')
           else:
-              GPIO.output(27,GPIO.LOW)
+             # GPIO.output(27,GPIO.LOW)
               print('Relay1-TurnOn-false')
 
       if message['path']== '/Blink':
-          global count1
           if message['data']==True:
-              count1+=1
-              thread1 = Relay1Thread(count1, "Relay1Thread")
-              thread1.start()
-              print('Relay1-Blink-true')
+             global checkRelay1
+             checkRelay1=True
+             t = threading.Thread(name='1',target=worker1)
+             t.start()          
+             print('Relay1-Blink-true')
           else:
+              global checkRelay1
+              checkRelay1=False
               print('Relay1-Blink-false')
 #Relay2
 def stream_relay2(message):
@@ -103,13 +83,15 @@ def stream_relay2(message):
               print('Relay2-TurnOn-false')
 
     if message['path']== '/Blink':
-         global count2
          if message['data']==True:
-              count2+=2
-              thread2 = Relay2Thread(count2, "Relay2Thread")
-              thread2.start()
-              print('Relay2-Blink-true')
+             global checkRelay2
+             checkRelay2=True
+             t = threading.Thread(name='2',target=worker2)
+             t.start()  
+             print('Relay2-Blink-true')
          else:
+              global checkRelay2
+              checkRelay2=False
               print('Relay2-Blink-false')
 #Relay3
 def stream_relay3(message):
--- thread.py
+++ thread.py
@@ -1,46 +1,75 @@
 import threading
 import time
+from queue import Queue
 
-count1= 0
-count2= 0
+# count1= 0
+# count2= 0
 
 
-class Relay1Thread (threading.Thread):
-     def __init__(self,threadID, name):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
+# class Relay1Thread (threading.Thread):
+#      def __init__(self,threadID, name):
+#         threading.Thread.__init__(self)
+#         self.threadID = threadID
+#         self.name = name
 
-     def run(self):
-        blinkRelay1(self.name)
+#      def run(self):
+#         blinkRelay1(self.name)
 
-class Relay2Thread (threading.Thread):
-     def __init__(self,threadID, name):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
+# class Relay2Thread (threading.Thread):
+#      def __init__(self,threadID, name):
+#         threading.Thread.__init__(self)
+#         self.threadID = threadID
+#         self.name = name
 
-     def run(self):
-        blinkRelay2(self.name)
+#      def run(self):
+#         blinkRelay2(self.name)
 
-def blinkRelay1(threadName):
-   global count1
-   while count1<=5:
-      print('relay 1')
-      count1+=1
-      time.sleep(1)
+# def blinkRelay1(threadName):
+#    global count1
+#    while count1<=5:
+#       print('relay 1')
+#       count1+=1
+#       time.sleep(1)
 
-def blinkRelay2(threadName):
-       global count2
-       while count2<=5:
-        print('relay 2')
-        count2+=1
+# def blinkRelay2(threadName):
+#        global count2
+#        while count2<=5:
+#         print('relay 2')
+#         count2+=1
+#         time.sleep(1)
+
+# # Create new threads
+
+# threadList = ["Thread-1", "Thread-2", "Thread-3"]
+# threads = []
+# threadID = 1
+# workQueue = Queue(10)
+
+# for tName in threadList:
+#        thread1 = Relay1Thread(threadID, "Relay1Thread",workQueue)
+#        thread1.start()
+#        threads.append(thread1)
+#        threadID += 1
+count=0
+
+def worker():
+    global count
+    while count<=5:
+        print('worker')
+        count+=1
+        if count==5:
+           count=0
+           t2 = threading.Thread(name='2',target=worker)
+           t2.start()
+           print('stop')
+
         time.sleep(1)
 
-# Create new threads
-thread1 = Relay1Thread(1, "Relay1Thread")
-thread2 = Relay2Thread(2, "Relay2Thread")
-
-# Start new Threads
-thread1.start()
-thread2.start()
+def worker1():
+    global count
+    while count<=5:
+        print('worker')
+        time.sleep(1)
+    
+t = threading.Thread(name='1',target=worker)
+t.start()
