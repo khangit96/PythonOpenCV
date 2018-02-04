e3ab382fbca8678319f280c23e8fc9d00849bc53
tdmuteam <tdmuteam2016@gmail.com>
2018-02-04 12:23:13 +0000

    djsjfd

---
+++ VAForSmartHome/main.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import sys
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+
+face_cascade = cv2.CascadeClassifier('../haarcascade_frontalface_default.xml')
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+ 
+time.sleep(0.1)
+ 
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	
+    img = frame.array
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        
+    for (x,y,w,h) in faces:
+        print('Detect face')
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        
+    cv2.imshow("Frame", img)
+    key = cv2.waitKey(1) & 0xFF
+ 
+    rawCapture.truncate(0)
+ 
+    if key == ord("q"):
+	    break
